db165325e008be171df2d1bb9eebc52676831caf
Benny Lichtner <bennlich@gmail.com>
2012-12-29 17:56:07 -0700

    New version with gui.

--- circledude2.js
+++ circledude2.js
@@ -1,9 +1,10 @@
 var myCanvas, myCtx;
 
-var listOfCircleDudes = [];
+var circleDudeCtrl;
 
-var NUM_CIRCLE_DUDES = 30;
-var FISHBOWL_RADIUS = 100;
+var listOfCircleDudes = [];
+var intersections = {};
+
 var WIDTH, HEIGHT;
 
 function init() {
@@ -13,39 +14,142 @@ function init() {
 
 	myCtx = myCanvas.getContext("2d");
 
-	for (var i = 0; i < NUM_CIRCLE_DUDES; i++) {
-		listOfCircleDudes.push(new CircleDude());
-	}
+	circleDudeCtrl = new CircleDudeController();
 
 	myCtx.fillStyle = "rgba(0,0,0)";
 	myCtx.fillRect(0,0,WIDTH,HEIGHT);
 
-	setInterval(step,50);
+	var gui = new dat.GUI();
+	
+	var f1 = gui.addFolder('Draw Mode');
+	f1.add(circleDudeCtrl, 'bgAlpha', 0.00, 1.00).step(0.01);
+	f1.add(circleDudeCtrl, 'drawCircleDudes');
+	f1.add(circleDudeCtrl, 'drawIntersections');
+	f1.add(circleDudeCtrl, 'drawRadii');
+	f1.add(circleDudeCtrl, 'drawTails');
+	f1.add(circleDudeCtrl, 'tailLen',1,20).step(1);
+
+	var f2 = gui.addFolder('Circle Dudes')
+	f2.add(circleDudeCtrl, 'NUM_CIRCLE_DUDES', 1, 100);
+	f2.add(circleDudeCtrl, 'centerAttractionStrength',0,0.5).step(0.01);
+
+	f1.open();
+	f2.open();
+
+	setInterval(function() { circleDudeCtrl.step(); },50);
 }
 
-function step() {
-	myCtx.fillStyle = "rgba(0,0,0,0.05)";
-	myCtx.fillRect(0,0,WIDTH,HEIGHT);
-	for (var i = 0; i < listOfCircleDudes.length; i++) {
-		listOfCircleDudes[i].step();
-		// listOfCircleDudes[i].draw();
-		for (var j = i+1; j < listOfCircleDudes.length; j++) {
-			if (j != i) {
-				var hotspots = intersection(listOfCircleDudes[i], listOfCircleDudes[j]);
-				var circleDudeDistance = listOfCircleDudes[i].pos.distanceFrom(listOfCircleDudes[j].pos);
-				var col = Math.round(circleDudeDistance*255/(listOfCircleDudes[i].r+listOfCircleDudes[j].r));
-				for (var k = 0; k < hotspots.length; k++) {
-					myCtx.beginPath();
-					// myCtx.arc(listOfCircleDudes[i].pos.e(1)+hotspots[k][0], listOfCircleDudes[i].pos.e(2)+hotspots[k][1], 1, 0, Math.PI*2, false);
-					myCtx.arc(hotspots[k][0], hotspots[k][1], 1, 0, Math.PI*2, false);
-					myCtx.closePath();
-					myCtx.fillStyle = "rgba(255,255,255,0.5)";
-					myCtx.fill();
+function CircleDudeController() {
+	this.NUM_CIRCLE_DUDES = 20;
+	this.FISHBOWL_RADIUS = 100;
+	this.drawCircleDudes = false;
+	this.drawIntersections = true;
+	this.drawRadii = false;
+	this.drawTails = false;
+	this.bgAlpha = 0.99;
+	this.centerAttractionStrength = 0.01;
+	this.tailLen = 5;
+
+
+	this.step = function() {
+
+		if (listOfCircleDudes.length > this.NUM_CIRCLE_DUDES) {
+			listOfCircleDudes.pop();
+		}
+		else if (listOfCircleDudes.length < this.NUM_CIRCLE_DUDES) {
+			listOfCircleDudes.push(new CircleDude(this));
+		}
+
+		myCtx.fillStyle = "rgba(0,0,0,"+this.bgAlpha+")";
+		myCtx.fillRect(0,0,WIDTH,HEIGHT);
+		for (var i = 0; i < listOfCircleDudes.length; i++) {
+			if (!intersections[i]) intersections[i] = {};
+			listOfCircleDudes[i].step();
+			if (this.drawCircleDudes) {
+				listOfCircleDudes[i].draw();
+			}
+			for (var j = i+1; j < listOfCircleDudes.length; j++) {
+				if (j != i) {					
+					var hotspots = intersection(listOfCircleDudes[i], listOfCircleDudes[j]);
+					if (!intersections[i][j]) intersections[i][j] = [];
+					for (var k = 0; k < hotspots.length; k++) {
+						if (k >= intersections[i][j].length) intersections[i][j].push([]);
+						intersections[i][j][k].push([hotspots[k][0], hotspots[k][1]]);
+						if (intersections[i][j][k].length > this.tailLen) {
+							intersections[i][j][k].shift();
+						}
+						if (this.drawIntersections) {
+							myCtx.beginPath();
+							myCtx.arc(hotspots[k][0], hotspots[k][1], 1, 0, Math.PI*2, false);
+							myCtx.closePath();
+							myCtx.fillStyle = "rgba(255,255,255,0.5)";
+							myCtx.fill();
+						}
+						if (this.drawRadii) {
+							myCtx.beginPath();
+							myCtx.moveTo(hotspots[k][0],hotspots[k][1]);
+							myCtx.lineTo(listOfCircleDudes[i].pos.e(1),listOfCircleDudes[i].pos.e(2));
+							myCtx.moveTo(hotspots[k][0],hotspots[k][1]);
+							myCtx.lineTo(listOfCircleDudes[j].pos.e(1),listOfCircleDudes[j].pos.e(2));
+							myCtx.closePath();
+							myCtx.strokeStyle = "rgba(255,255,255,0.5)";
+							myCtx.stroke();
+						}
+					}
+					if (this.drawTails) {
+						for (var k = 0; k < intersections[i][j].length; k++) {
+							if (intersections[i][j][k].length && intersections[i][j][k].length > 0) {
+								myCtx.beginPath();
+								myCtx.moveTo(intersections[i][j][k][0][0],intersections[i][j][k][0][1]);
+								for (var l = 1; l < intersections[i][j][k].length; l++) {
+									myCtx.lineTo(intersections[i][j][k][l][0],intersections[i][j][k][l][1]);
+								}
+								myCtx.strokeStyle = "rgba(255,255,255,0.5)";
+								myCtx.stroke();
+							}
+						}
+					}
 				}
 			}
 		}
 	}
+
 }
+
+
+function CircleDude(ctrl) {
+	this.ctrl = ctrl;
+	this.tail = [];
+	this.pos = $V([Math.random()*WIDTH, Math.random()*HEIGHT]);
+	this.r = 50+Math.random()*300;
+	var maxSpeed = 5;
+	this.vel = $V([-maxSpeed/2+Math.random()*maxSpeed,-maxSpeed+Math.random()*maxSpeed]);
+
+	this.angVel = Math.random()*Math.PI*2/100; // velocity of the tater
+	this.theta = 0; // position of the tater
+}
+
+CircleDude.prototype.step = function() {
+	var wallVector = $V([WIDTH/2,HEIGHT/2]).subtract(this.pos).x(this.ctrl.centerAttractionStrength);
+	this.vel = this.vel.rotate(posOrNeg()*Math.random()*Math.PI*2/16,$V([0,0]));
+	
+	this.pos = this.pos.add(this.vel);
+	this.pos = this.pos.add(wallVector);
+
+	this.theta = (this.theta + this.angVel) % (Math.PI * 2);
+}
+
+CircleDude.prototype.draw = function() {
+	myCtx.beginPath();
+	myCtx.arc(this.pos.e(1), this.pos.e(2), this.r, 0, Math.PI*2, false);
+	myCtx.closePath();
+	myCtx.strokeStyle = "rgb(255,255,255)";
+	myCtx.stroke();
+}
+
+/////////////////
+////T O O L S////
+/////////////////
 
 function posOrNeg() {
 	return Math.random() > 0.5 ? 1 : -1;
@@ -69,34 +173,6 @@ function hsv_to_rgb(h, s, v) {
 	return [255 * (rgb[0] + m), 255 * (rgb[1] + m), 255 * (rgb[2] + m)];  
 }
 
-function CircleDude() {
-
-	this.tail = [];
-	this.pos = $V([Math.random()*WIDTH, Math.random()*HEIGHT]);
-	this.r = 50+Math.random()*300;
-	var maxSpeed = 5;
-	this.vel = $V([-maxSpeed/2+Math.random()*maxSpeed,-maxSpeed+Math.random()*maxSpeed]);
-
-	this.angVel = Math.random()*Math.PI*2/100; // velocity of the tater
-	this.theta = 0; // position of the tater
-
-	this.step = function() {
-		var wallVector = $V([WIDTH/2,HEIGHT/2]).subtract(this.pos).x(1/100);
-		console.log(wallVector.modulus());
-		this.vel = this.vel.rotate(posOrNeg()*Math.random()*Math.PI*2/16,$V([0,0]));
-		
-		this.pos = this.pos.add(this.vel).add(wallVector);
-		this.theta = (this.theta + this.angVel) % (Math.PI * 2);
-	}
-
-	this.draw = function() {
-		myCtx.beginPath();
-		myCtx.arc(this.pos.e(1), this.pos.e(2), this.r, 0, Math.PI*2, false);
-		myCtx.closePath();
-		myCtx.stroke(0,0,0);
-	}
-
-}
 
 // totally didn't copy from http://stackoverflow.com/questions/12219802/a-javascript-function-that-returns-the-x-y-points-of-intersection-between-two-ci
 function intersection(circleDude1, circleDude2) {
